b0f2209fe97b58f6a64e37284ec491ffd2604d17
bmik98 <bmik9898@gmail.com>
2016-02-01 23:11:14 +0300

    the first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import stack.GenericStack;
+import stack.StackException;
+
 public class Main {
     public static void main(String[] args) {
-	// write your code here
+        GenericStack<String> stack = new GenericStack<>(4);
+
+        try {
+            stack.push("First");
+            stack.push("Second");
+            stack.push("Third");
+            stack.push("Fourth");
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+            System.out.println(stack.peek());
+            System.out.println(stack.peek());
+            System.out.println(stack.peek());
+        } catch (StackException e) {
+            e.printStackTrace();
+        }
     }
 }
